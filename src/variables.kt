bc5b4e5065c2928678eed93526d0f619a48f91d6
unknown <gurupatoh>
2021-08-28 10:54:45 +0300

    first commit

---
+++ src/variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val name:String = "patrick " //immutable
+    var marriageStatus:String? = null
+    var height:Int = 23 // mutable
+    height=34
+
+    println(name)
+    println(height)
+    println(marriageStatus)
+    println(name+height)
+
+}
